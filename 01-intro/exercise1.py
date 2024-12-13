aea9d42ed10b117b1fb48a332d1630a06147b3e2
paolo <paolo.prandoni@epfl.ch>
2024-12-13 18:10:56 +0100

    cleanup

--- 01-intro/exercise1.py
+++
@@ -1,6 +0,0 @@
-import base64
-
-encrypted_message = b'Vm0weE1HRXlWbFJsYkZaVFlteGthVmx0ZUV0V01WbDNXa2M1VjFadGVGZFNNbEYzVUZFOVBRPT0='
-message = base64.b64decode(encrypted_message).decode('utf-8')
-
-print(message)
